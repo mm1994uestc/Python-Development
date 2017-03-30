6a25a9b9988675cbda2432e36c16fcc6daf6e04e
mm1994uestc <547238541@qq.com>
2017-03-31 00:37:48 +0800

    Opencv Learning!

--- Opencv_Test/Opencv_Test.py
+++ Opencv_Test/Opencv_Test.py
@@ -284,3 +284,18 @@ import cmath
 # cv2.waitKey(0)
 # cv2.destroyWindow('image')
 
+Cap = cv2.VideoCapture('BlueCapture.mp4')
+while 1:
+    ret, frame = Cap.read()
+    blue = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    l = np.array([100, 50, 50])
+    h = np.array([180, 255, 255])
+    mask = cv2.inRange(blue, l, h)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
